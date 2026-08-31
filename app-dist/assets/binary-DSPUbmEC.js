8234c91e98bb674be21b3d40f753bf23b5e6269f
educationlessonplans <teachtylerhenley@gmail.com>
2026-08-31 10:17:30 +0900

    include verified mounted app artifact

---
+++ app-dist/assets/binary-DSPUbmEC.js
@@ -0,0 +1,1 @@
+import{B as r}from"./BinaryModule-Aj1vLnwf-BAG91qdf.js";const a=""+new URL("/inkwell/app/assets/harper_wasm_bg-LWXDb7Ll.wasm",import.meta.url).href,l=r.create(a,"full");export{l as binary};
