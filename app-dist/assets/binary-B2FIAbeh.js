9adf24acd781478d539c9794b17df58ba92ca0ec
educationlessonplans <teachtylerhenley@gmail.com>
2026-09-02 23:24:59 +0900

    Fix loadUiMotionPreference and Settings drawer imports in app bundle

--- app-dist/assets/binary-B2FIAbeh.js
+++
@@ -1,1 +0,0 @@
-import{B as r}from"./BinaryModule-Aj1vLnwf-BBOMxyrG.js";import"./index-DDWi982s.js";import"./tiptap-Cn9Z1Ajg.js";import"./nlp-CAQbg9o9.js";const a=""+new URL("/inkwell/app/assets/harper_wasm_bg-LWXDb7Ll.wasm",import.meta.url).href,p=r.create(a,"full");export{p as binary};
