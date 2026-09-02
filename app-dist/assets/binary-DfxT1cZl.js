a72aa6c20ebdc563df2567b5e6f35a7a4a68047b
educationlessonplans <teachtylerhenley@gmail.com>
2026-09-02 22:13:37 +0900

    Fix Supabase URL static inlining and make Pricing and magic link visible in app header

--- app-dist/assets/binary-DfxT1cZl.js
+++
@@ -1,1 +0,0 @@
-import{B as r}from"./BinaryModule-Aj1vLnwf-D2fYlShb.js";import"./index-BgdaECxV.js";import"./tiptap-Cn9Z1Ajg.js";import"./nlp-CAQbg9o9.js";const a=""+new URL("/inkwell/app/assets/harper_wasm_bg-LWXDb7Ll.wasm",import.meta.url).href,p=r.create(a,"full");export{p as binary};
