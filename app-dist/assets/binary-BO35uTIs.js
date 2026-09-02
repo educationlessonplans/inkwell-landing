3085dfd82b9382d08399e0e6e4f9ff170601b1ed
educationlessonplans <teachtylerhenley@gmail.com>
2026-09-02 23:14:24 +0900

    Include updated PWA service worker to refresh client cache automatically

---
+++ app-dist/assets/binary-BO35uTIs.js
@@ -0,0 +1,1 @@
+import{B as r}from"./BinaryModule-Aj1vLnwf-Bk8faMQl.js";import"./index-BYHnGK6u.js";import"./tiptap-Cn9Z1Ajg.js";import"./nlp-CAQbg9o9.js";const a=""+new URL("/inkwell/app/assets/harper_wasm_bg-LWXDb7Ll.wasm",import.meta.url).href,p=r.create(a,"full");export{p as binary};
